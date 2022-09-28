f2110fb7bbb08146111f8406d291b72a45576a24
a88019401 <a88019401@yahoo.com.tw>
2022-09-28 21:24:04 +0800

    W04-P3: W03 menu demo, adding filter function for categories

---
+++ demo/w04_hooks/src/App_28.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T11_28 from './tutorial/1-useState/T13_28-useState-array';
+
+const App_28 = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial 13 - 909410028</h2>
+      <T11_28 />
+    </div>
+  );
+};
+
+export default App_28;
---
+++ demo/w04_hooks/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_28 from './App_28';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App_28 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
