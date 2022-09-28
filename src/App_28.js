11eb5fc4f46658cd474ca92baef61559b38d5d62
a88019401 <a88019401@yahoo.com.tw>
2022-09-28 20:42:02 +0800

    Hooks Tutorial T13_28, useState using array of objects wit remove item capability

--- src/App_28.js
+++ src/App_28.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import T11_28 from './tutorial/1-useState/T12_28-useState-basics';
+import T11_28 from './tutorial/1-useState/T13_28-useState-array';
 
 const App_28 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 12 - 909410028</h2>
+      <h2>Tutorial 13 - 909410028</h2>
       <T11_28 />
     </div>
   );
