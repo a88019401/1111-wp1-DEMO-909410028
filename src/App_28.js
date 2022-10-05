a6eb1c344ec906c237197965e7f45cc061f5979b
a88019401 <a88019401@yahoo.com.tw>
2022-10-05 20:25:14 +0800

    W05-P3: T23_28 fetch data from Github and display them

--- src/App_28.js
+++ src/App_28.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T21_28 from './tutorial/2-useEffect/T21_28-useEffect-basics';
+import T23_28 from './tutorial/2-useEffect/T23_28-useEffect-fetch-data';
 
 const App_28 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 21_909410028</h2>
-      <T21_28 />
+      <h2>Tutorial 23_909410028</h2>
+      <T23_28 />
     </div>
   );
 };
