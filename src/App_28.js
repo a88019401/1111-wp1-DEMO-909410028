63b1c238f1c136088723e4dea6e64609b23076a7
a88019401 <a88019401@yahoo.com.tw>
2022-10-05 19:46:23 +0800

    W05-P2: useEffect 3 cases demo

--- src/App_28.js
+++ src/App_28.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T11_28 from './tutorial/1-useState/T14_28-useState-object';
+import T21_28 from './tutorial/2-useEffect/T21_28-useEffect-basics';
 
 const App_28 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 14 - 909410028</h2>
-      <T11_28 />
+      <h2>Tutorial 21_909410028</h2>
+      <T21_28 />
     </div>
   );
 };
