b06c4f392ac672a65cbbff0fa23ae524cab5df49
a88019401 <a88019401@yahoo.com.tw>
2022-10-12 19:41:36 +0800

    W06-P1: Use DevTools to exam useState values

--- src/App_28.js
+++ src/App_28.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import T31_28 from './tutorial/3-conditional-rendering/T31_28-multiple-returns';
+import React, { useState, useEffect } from 'react'
+import List_28 from './components/List_28'
+import Alert_28 from './components/Alert_28'
 
 const App_28 = () => {
-  return (
-    <div className='container'>
-      <h2>Tutorial 31_909410028</h2>
-      <T31_28 />
-    </div>
-  );
-};
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [alert, setAlert] = useState({
+    show: false, msg:'', type: '',
+  });
 
-export default App_28;
+    const showAlert = (show = false, msg= '', type='') => {
+      setAlert({show, msg, type});
+    }
+  const handleSubmit = (e) => {
+    e.preventDefault(); //預先取消掉 action 的動作
+    if (!name){
+      showAlert(true,'Please enter value','danger')
+    }else{
+      showAlert(true,'value changed','success');
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  return <>
+  <section className='section-center'>
+    <form className="grocery-form" onSubmit = {handleSubmit}>
+      <h3>Grocery Bub - 909410028</h3>
+      <div className='form-control'>
+        <input type="text" className='grocery' placeholder='e.g. eggs' value ={name} onChange={
+          (e) => {
+            setName(e.target.value)
+            }}/> 
+        <button type='submit' className='submit-btn' >submit</button>
+      </div>
+    </form>
+  </section>
+  </>
+}
+
+export default App_28
