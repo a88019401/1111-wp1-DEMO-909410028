30f1fbf5aea8104782bcf132a4e2d8e514eb44f1
a88019401 <a88019401@yahoo.com.tw>
2022-11-02 20:59:06 +0800

    W09-P3: find a product using productId using your last digit of ID

--- src/App_28.js
+++ src/App_28.js
@@ -5,6 +5,8 @@ import AboutPage_28 from './pages/AboutPage_28';
 import ProductsPage_28 from './pages/ProductsPage_28';
 import ErrorPage_28 from './pages/ErrorPage_28';
 import HomePage_28 from './pages/HomePage_28';
+import SharedProductLayout_28 from './pages/SharedProductLayout_28';
+import SingleProductPage_28 from './pages/SingleProductPage_28';
 function App_28() {
   return (
     <>
@@ -13,10 +15,15 @@ function App_28() {
           <Route path='/' element={<SharedLayout_28 />}>
             <Route index element={<HomePage_28></HomePage_28>} />
             <Route path='about' element={<AboutPage_28></AboutPage_28>} />
-            <Route
-              path='products'
-              element={<ProductsPage_28></ProductsPage_28>}
-            />
+
+            <Route path='products' element={<SharedProductLayout_28 />}>
+              <Route index element={<ProductsPage_28></ProductsPage_28>} />
+              <Route
+                path=':productId'
+                element={<SingleProductPage_28 />}
+              ></Route>
+            </Route>
+
             <Route path='*' element={<ErrorPage_28></ErrorPage_28>} />
           </Route>
         </Routes>
---
+++ src/pages/SharedProductLayout_28.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+const SharedProductLayout_28 = () => {
+  return (
+    <>
+      <h2>Products</h2>
+      <Outlet />
+    </>
+  );
+};
+
+export default SharedProductLayout_28;
