6a65cb4bb4db45c5c684bdb651c9b1bd5d23f3d9
a88019401 <a88019401@yahoo.com.tw>
2022-11-16 21:00:50 +0800

    ### W11-P3_xx: for midterm P3c

--- src/components/Product_28.js
+++ src/components/Product_28.js
@@ -1,7 +1,17 @@
-const Product_28 = () => {
+const Product_28 = ({id,img,name,price}) => {/*{id,img,name,price}是解構 */
   return (
     <>
-      <h2>Product</h2>
+    <div className="single-product">
+               <img
+                 src={img}
+                 className="single-product-img img"
+                 alt={name}
+               />
+               <footer>
+                 <h5 className="name">{name}</h5>
+                 <span className="price">{price}</span>
+               </footer>
+             </div>
     </>
   );
 };
