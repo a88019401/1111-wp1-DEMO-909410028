7bc6569b4f0f09624aaa342566c915b34c352db1
a88019401 <a88019401@yahoo.com.tw>
2022-11-23 21:03:24 +0800

    ### W12-P3: install sass and convert index.css to index.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App_28 from './App_28';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
