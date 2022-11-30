e36de18cd2c4e4a681337dbb63033441ceee2227
a88091401 <a88019401@yahoo.com.tw>
2022-11-30 19:29:57 +0800

    ### W13-P1: PersonContext for T82_28

--- src/App_28.js
+++ src/App_28.js
@@ -1,34 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import T81_28 from './tutorial/8-useContext/T81_28-contextAPI';
 
-import HomePage_28 from './pages/HomePage_28';
-import ErrorPage_28 from './pages/ErrorPage_28';
-import SharedLayout_28 from './pages/SharedLayout_28';
-
-import P1Page_28 from './pages/P1Page_28';
-import P2Page_28 from './pages/P2Page_28';
-import P3Page_28 from './pages/P3Page_28';
-import P4Page_28 from './pages/P4Page_28';
-import P5Page_28 from './pages/P5Page_28';
-import P6Page_28 from './pages/P6Page_28';
-
-function App() {
+const App_28 = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SharedLayout_28 />}>
-          <Route path='p1_28' element={<P1Page_28 />} />
-          <Route path='p2_28' element={<P2Page_28 />} />
-          <Route path='p3_28' element={<P3Page_28 />} />
-          <Route path='p4_28' element={<P4Page_28 />} />
-          <Route path='p5_28' element={<P5Page_28 />} />
-          <Route path='p6_28' element={<P6Page_28 />} />
-          <Route index element={<HomePage_28 />} />
-          <Route path='*' element={<ErrorPage_28 />} />
-
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className='container'>
+      <h2>Tutorial 81_909410028</h2>
+      <T81_28 />
+    </div>
   );
-}
+};
 
-export default App;
+export default App_28;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App_28 from './App_28';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App_28 />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
