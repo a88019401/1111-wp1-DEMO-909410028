4f68c2cef4fb582cdd873e6955d2da3ef7dd977a
a88091401 <a88019401@yahoo.com.tw>
2022-11-30 20:49:49 +0800

    ### W13-P2: Apply useReducer to add an item with modal alert

--- src/App_28.js
+++ src/App_28.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T81_28 from './tutorial/8-useContext/T81_28-contextAPI';
+import T61_28 from './tutorial/6-useReducer/T6_28-useReducer';
 
 const App_28 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 81_909410028</h2>
-      <T81_28 />
+      <h2>Tutorial T6_909410028</h2>
+      <T61_28 />
     </div>
   );
 };
